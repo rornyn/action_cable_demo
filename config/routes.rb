595ad91e9287e0da8e349a7ffb2349a5ebefae80
NayanGupta <ngupta@isystango.com>
2018-05-11 19:39:30 +0530

    chatroom and message setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+
+  resources :chat_rooms, only: [:index, :new, :create, :show ]
+
   root to: 'visitors#index'
   devise_for :users
   resources :users
+
+  mount ActionCable.server => '/cable'
+
 end
