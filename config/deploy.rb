528853d61760b8ac367572f4869492cb28070001
NayanGupta <ngupta@isystango.com>
2018-05-18 17:31:33 +0530

    minor fixes

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,8 @@ server '192.168.1.219', roles: [:web, :app, :db], primary: true
 set :application, "action_cable"
 set :repo_url, "git@github.com:shailjasingh/action_cable_demo.git"
 set :user, 'ubox55'
+set :password, 'ubox55'
+
 
 
 # Default branch is :master
@@ -29,8 +31,66 @@ set :stage,           :production
 set :deploy_via,      :remote_cache
 
 
-set :puma_conf, "#{shared_path}/config/puma.rb"
+# set :puma_conf, "#{shared_path}/config/puma.rb"
 
+
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
+
+# Don't change these unless you know what you're doing
+set :pty,             true
+set :use_sudo,        false
+set :stage,           :production
+set :deploy_via,      :remote_cache
+
+set :puma_bind,       "unix://#{shared_path}/sockets/action_cable_puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+
+
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/action_cable.pub) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, false  # Change to true if using ActiveRecord
+
+## Defaults:
+# set :scm,           :git
+# set :branch,        :master
+# set :format,        :pretty
+# set :log_level,     :debug
+# set :keep_releases, 5
+
+## Linked Files & Directories (Default None):
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+
+namespace :deploy do
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
+
+  after  :finishing,    :compile_assets
+  after  :finishing,    :restart
+end
+
+# ps aux | grep puma    # Get puma pid
+# kill -s SIGUSR2 pid   # Restart puma
+# kill -s SIGTERM pid   # Stop puma
 
 
 # # Default value for :linked_files is []
@@ -40,7 +100,7 @@ set :puma_conf, "#{shared_path}/config/puma.rb"
 # set :linked_dirs, ["log", "tmp", "public"]
 
 ## Linked Files & Directories (Default None):
-set :linked_files, %w{config/database.yml config/secrets.yml config/puma.rb}
+set :linked_files, %w{config/database.yml config/secrets.yml config/cable.yml}
 
 set :linked_dirs,  %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
@@ -68,11 +128,10 @@ set :linked_dirs,  %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/sy
 #   # before :start, :make_dirs
 # end
 
-namespace :deploy do
-  before 'check:linked_files', 'puma:config'
-  before 'check:linked_files', 'puma:nginx_config'
-  after 'puma:start', 'nginx:restart'
-end
+# namespace :deploy do
+#   before 'check:linked_files', 'puma:config'
+#   after 'puma:start'
+# end
 
 # ps aux | grep puma    # Get puma pid
 # kill -s SIGUSR2 pid   # Restart puma
